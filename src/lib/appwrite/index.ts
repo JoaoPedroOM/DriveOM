2d8506c5784407d1652e151543fa688b293b30b7
João Pedro <63527881+JoaoPedroOM@users.noreply.github.com>
2025-01-02 17:41:08 -0300

    feat: Iniciada implementação de criação de contas com envio de OTP

--- src/lib/appwrite/index.ts
+++ src/lib/appwrite/index.ts
@@ -1,5 +1,7 @@
+"use server";
+
 import { Account, Avatars, Client, Databases, Storage } from "node-appwrite";
-import { appwriteConfig } from "./config";
+import { appwriteConfig } from "@/lib/appwrite/config";
 import { cookies } from "next/headers";
 
 export const createSessionClient = async () => {
@@ -9,9 +11,7 @@ export const createSessionClient = async () => {
 
   const session = (await cookies()).get("appwrite-session");
 
-  if (!session || !session.value) {
-    throw new Error("No session");
-  }
+  if (!session || !session.value) throw new Error("No session");
 
   client.setSession(session.value);
 
@@ -19,30 +19,30 @@ export const createSessionClient = async () => {
     get account() {
       return new Account(client);
     },
-    get database() {
+    get databases() {
       return new Databases(client);
     },
   };
 };
 
 export const createAdminClient = async () => {
-    const client = new Client()
-      .setEndpoint(appwriteConfig.endpointUrl)
-      .setProject(appwriteConfig.projectId)
-      .setKey(appwriteConfig.secretKey);
-  
-    return {
-      get account() {
-        return new Account(client);
-      },
-      get databases() {
-        return new Databases(client);
-      },
-      get storage() {
-        return new Storage(client);
-      },
-      get avatars() {
-        return new Avatars(client);
-      },
-    };
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId)
+    .setKey(appwriteConfig.secretKey);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+    get storage() {
+      return new Storage(client);
+    },
+    get avatars() {
+      return new Avatars(client);
+    },
   };
+};
